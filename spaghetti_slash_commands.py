b89a99904e7d7493e1ac99660c11e6ac06b9c437
LonelyPenguin <86080520+LonelyPenguin@users.noreply.github.com>
2021-08-08 15:20:13 -0700

    use Interaction.data to send the user's message back to them

--- spaghetti_slash_commands.py
+++ spaghetti_slash_commands.py
@@ -8,20 +8,21 @@ class SpaghettiSlashCommands(commands.Cog):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_interaction(self, interaction):
-        if interaction.id != 873001414094377050:
+    async def on_interaction(self, interaction: discord.Interaction):
+
+        if interaction.data['id'] != '873001414094377050':
             return
+        command_content = interaction.data['options'][0]['value']
         async with aiohttp.ClientSession() as session:
-            url = f'https://discord.com/api/v8/interactions/{interaction.id}/{interaction.token}/callback'
+            url = f'https://discord.com/api/v9/interactions/{interaction.id}/{interaction.token}/callback'
             json = {
                 'type': 4,
                 'data': {
-                    'content': f'Hello {interaction.user.mention}!',
+                    'content': f'Hello {interaction.user.mention}! You said:\n```{command_content}```',
                     'flags': 64
                 }
             }
             await session.post(url, json=json)
-        print('interaction recieved')
 
 def setup(bot: commands.Bot):
     bot.add_cog(SpaghettiSlashCommands(bot))
